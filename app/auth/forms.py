d3382055543692619ae0c4cdf2479f20e67d9ee5
Albert Manya <albert.manya@schibsted.com>
2017-04-26 14:00:56 +0200

    Email not required in edit profile

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,8 +14,7 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
-                                           Email()])
+    email = StringField('Email')
     username = StringField('Username', validators=[
         Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Usernames must have only letters, '
@@ -24,10 +23,6 @@ class RegistrationForm(FlaskForm):
         Required(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[Required()])
     submit = SubmitField('Register')
-
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
 
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
--- app/main/forms.py
+++ app/main/forms.py
@@ -18,8 +18,7 @@ class EditProfileForm(FlaskForm):
 
 
 class EditProfileAdminForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField('Email')
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                               'Usernames must have only letters, '
