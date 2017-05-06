a62ef936349b625d9cc16b00a6886b8e65a014a0
Albert Manya <albert.manya@schibsted.com>
2017-05-06 11:07:37 +0200

    Students can't login

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,7 +31,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is not None and user.verify_password(form.password.data):
+        if user is not None and user.verify_password(form.password.data) and not user.is_student():
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
