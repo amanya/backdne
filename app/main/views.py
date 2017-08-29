90a878c7342ace7de5dcb72e69478a10eb4eacf6
Albert Manya <albert.manya@schibsted.com>
2017-08-29 15:10:30 +0200

    User shouldn't be visible if not logged in

--- app/main/views.py
+++ app/main/views.py
@@ -41,6 +41,7 @@ def index():
 
 
 @main.route('/user/<username>')
+@login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
